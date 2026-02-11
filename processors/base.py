f87b6b4bf0de7d14e0fa82dd836f3e21fac00610
Irina <iballesteroso@unicartagena.edu.co>
2026-02-11 13:32:57 -0500

    Limpieza repo: solo landing pública

--- processors/base.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-class DataProcessor:
-    def process(self, df: pd.DataFrame) -> pd.DataFrame:
-        raise NotImplementedError("Cada módulo debe implementar su propio método process")
